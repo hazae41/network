9f961d59feeb31876dca47f6bb1ba2dd995514bb
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-15 23:59:58 +0100

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 uint256 constant U256_MAX = (2 ** 256) - 1;
 
